5cbf9e1adccf6961e8d8255f17978a48ccabd893
tobyatgithub <toby.fangyuan@gmail.com>
2019-01-28 20:41:10 -0800

    adding new budget working.

--- budget_tool/budgets/forms.py
+++ budget_tool/budgets/forms.py
@@ -1,1 +1,14 @@
-from django.forms
+from django.forms import ModelForm
+from .models import Budget, Transaction
+
+
+class BudgetForm(ModelForm):
+    class Meta:
+        model = Budget
+        fields = ['name', 'total_budget', 'user']
+
+
+class TransactionForm(ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ['assigned_to', 'budget', 'trans_type', 'amount']
--- budget_tool/budgets/urls.py
+++ budget_tool/budgets/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import BudgetDetailView, BudgetListView, TransactionView
+from .views import BudgetDetailView, BudgetListView, TransactionView, BudgetCreateView
 
 urlpatterns = [
     path('budget', BudgetListView.as_view(), name='budget_list_view'),
     path('budgets', BudgetDetailView.as_view(), name='budget_detail_view'),
     path('transaction/<int:id>', TransactionView.as_view(), name='transaction_detail'),
+    path('budget/add', BudgetCreateView.as_view(), name='budget_add'),
     # path('card/<int:id>', CardView.as_view(), name='card_detail'),
 ]
